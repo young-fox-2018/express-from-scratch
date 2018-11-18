f22bd7f8d20d2ea2551700604abdb086c5f35877
maitreya <bluespider529@gmail.com>
2018-11-18 13:07:43 +0700

    done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 "use strict"
+
 const express = require("express");
 const app = express();
 
 const routes = require("./routes");
-const student = require("./routes/student");
-const teacher = require("./routes/teacher");
+const student = require("./routes/student.js");
+const teacher = require("./routes/teacher.js");
 
 app.use("/", routes);
-app.use("/routes/student", student);
-app.use("/routes/teacher", teacher);
+app.use("/student", student);
+app.use("/teacher", teacher);
 
 app.listen(3000, () => {
     console.log("Running on local host...")
